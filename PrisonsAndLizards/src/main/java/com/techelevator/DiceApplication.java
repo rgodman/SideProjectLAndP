50141dcc526672d7bef001956a1ff2e78232b6f7
sadierudesyle <58157470+sadierudesyle@users.noreply.github.com>
2022-03-07 14:51:01 -0500

    won't accept one sided die, added natural roll result

--- PrisonsAndLizards/src/main/java/com/techelevator/DiceApplication.java
+++ PrisonsAndLizards/src/main/java/com/techelevator/DiceApplication.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,41 +9,53 @@ import java.util.Random;
 
 public class DiceApplication {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  throws IllegalArgumentException{
         Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to the dice application for Prisons and Lizards");
-        System.out.println("                 ___====-_  _-====___\n" +
-                "           _--^^^#####//      \\\\#####^^^--_\n" +
-                "        _-^##########// (    ) \\\\##########^-_\n" +
-                "       -############//  |\\^^/|  \\\\############-\n" +
-                "     _/############//   (@::@)   \\\\############\\_\n" +
-                "    /#############((     \\\\//     ))#############\\\n" +
-                "   -###############\\\\    (oo)    //###############-\n" +
-                "  -#################\\\\  / VV \\  //#################-\n" +
-                " -###################\\\\/      \\//###################-\n" +
-                "_#/|##########/\\######(   /\\   )######/\\##########|\\#_\n" +
-                "|/ |#/\\#/\\#/\\/  \\#/\\##\\  |  |  /##/\\#/  \\/\\#/\\#/\\#| \\|\n" +
-                "`  |/  V  V  `   V  \\#\\| |  | |/#/  V   '  V  V  \\|  '\n" +
-                "   `   `  `      `   / | |  | | \\   '      '  '   '\n" +
-                "                    (  | |  | |  )\n" +
-                "                   __\\ | |  | | /__\n" +
-                "                  (vvv(VVV)(VVV)vvv)");
+        System.out.println("""
+                                 ___====-_  _-====___
+                           _--^^^#####//      \\\\#####^^^--_
+                        _-^##########// (    ) \\\\##########^-_
+                       -############//  |\\^^/|  \\\\############-
+                     _/############//   (@::@)   \\\\############\\_
+                    /#############((     \\\\//     ))#############\\
+                   -###############\\\\    (oo)    //###############-
+                  -#################\\\\  / VV \\  //#################-
+                 -###################\\\\/      \\//###################-
+                _#/|##########/\\######(   /\\   )######/\\##########|\\#_
+                |/ |#/\\#/\\#/\\/  \\#/\\##\\  |  |  /##/\\#/  \\/\\#/\\#/\\#| \\|
+                `  |/  V  V  `   V  \\#\\| |  | |/#/  V   '  V  V  \\|  '
+                   `   `  `      `   / | |  | | \\   '      '  '   '
+                                    (  | |  | |  )
+                                   __\\ | |  | | /__
+                                  (vvv(VVV)(VVV)vvv)""");
+        System.out.println();
 
 
         System.out.println("Please use only whole numbers.    ");
+        System.out.println();
         Random random = new Random();
         System.out.print("How many dice would you like to roll?  ");
+        System.out.println();
         String diceQtyInput = userInput.nextLine();
         int numberOfDice = Integer.parseInt(diceQtyInput);
 
-        System.out.print("How many sides do we need?   ");
-        String manySidesOfDice = userInput.nextLine();
-        int sidesToDice = random.nextInt(1, Integer.parseInt(manySidesOfDice)) + 1;
 
+
+        System.out.print("How many sides do we need?   ");
+        System.out.println();
+        String manySidesOfDice = userInput.nextLine();
+//        int sidesToDice = random.nextInt(1, Integer.parseInt(manySidesOfDice)) + 1;
+
+        while (Integer.parseInt(manySidesOfDice) <= 1) {
+            System.out.println("ERROR There has to be at least 2 sides!");
+            System.out.print("How many sides do we need?   ");
+            manySidesOfDice = userInput.nextLine();
+        }
 
 
         System.out.print("What is your modifier?  Please include the + or - (example: +4 or -2):   ");
+        System.out.println();
         String plusOrMinusSkill = userInput.nextLine();
         int skillModifier = Integer.parseInt(plusOrMinusSkill);
 
@@ -62,6 +76,29 @@ public class DiceApplication {
 
         System.out.println("You rolled:  " +
                 "" + rollResult + "   Dice results: " + listOfSides + " + " +  " " + skillModifier);
+        System.out.println();
+
+        for (int side : listOfSides) {
+            if (numberOfDice == 1 && (side == 1 || side == 20)) {
+                System.out.println("""
+                                     __.-/|
+                                     \\`o_O'
+                                      =( )=  +-----+
+                                        U|   | OMG |
+                              /\\  /\\   / |   +-----+
+                             ) /^\\) ^\\/ _)\\     |
+                             )   /^\\/   _) \\    |
+                             )   _ /  / _)  \\___|_
+                         /\\  )/\\/ ||  | )_)\\___,|))
+                        <  >      |(,,) )__)    |
+                         ||      /    \\)___)\\
+                         | \\____(      )___) )____
+                          \\______(_______;;;)__;;;)""");
+                break;
+            }
+        }
+
+
 
 
 
