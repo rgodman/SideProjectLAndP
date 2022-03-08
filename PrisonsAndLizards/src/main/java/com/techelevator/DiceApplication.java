61677ca03207edc670adfc5de81c4c24de4735e8
Ryan Godman <ryan.godman@techelevator.com>
2022-03-08 10:50:36 -0500

    started the re-run-sort of

--- PrisonsAndLizards/src/main/java/com/techelevator/DiceApplication.java
+++ PrisonsAndLizards/src/main/java/com/techelevator/DiceApplication.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 public class DiceApplication {
 
-    public static void main(String[] args)  throws IllegalArgumentException{
+    public static void main(String[] args) throws IllegalArgumentException {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to the dice application for Prisons and Lizards");
         System.out.println("""
@@ -41,7 +41,6 @@ public class DiceApplication {
         int numberOfDice = Integer.parseInt(diceQtyInput);
 
 
-
         System.out.print("How many sides do we need?   ");
         System.out.println();
         String manySidesOfDice = userInput.nextLine();
@@ -62,7 +61,7 @@ public class DiceApplication {
         List<Integer> listOfSides = new ArrayList<>();
         if (Integer.parseInt(diceQtyInput) >= 1) {
             for (int i = 0; i < Integer.parseInt(diceQtyInput); i++) {
-                int newRandomNumber = random.nextInt(1,Integer.parseInt(manySidesOfDice) +1);
+                int newRandomNumber = random.nextInt(1, Integer.parseInt(manySidesOfDice) + 1);
                 listOfSides.add(newRandomNumber);
             }
         }
@@ -75,11 +74,11 @@ public class DiceApplication {
         }
 
         System.out.println("You rolled:  " +
-                "" + rollResult + "   Dice results: " + listOfSides + " + " +  " " + skillModifier);
+                "" + rollResult + "   Dice results: " + listOfSides + " + " + " " + skillModifier);
         System.out.println();
 
         for (int side : listOfSides) {
-            if (numberOfDice == 1 && (side == 1 || side == 20)) {
+            if (numberOfDice == 1 && (side == 1)) {
                 System.out.println("""
                                      __.-/|
                                      \\`o_O'
@@ -95,23 +94,50 @@ public class DiceApplication {
                          | \\____(      )___) )____
                           \\______(_______;;;)__;;;)""");
                 break;
+            } else {
+                if (numberOfDice == 1 && (side == 20)) {
+                    System.out.println("                            ==(W{==========-      /===-                        \n" +
+                            "                              ||  (.--.)         /===-_---~~~~~~~~~------____  \n" +
+                            "                              | \\_,|**|,__      |===-~___                _,-' `\n" +
+                            "                 -==\\\\        `\\ ' `--'   ),    `//~\\\\   ~~~~`---.___.-~~      \n" +
+                            "             ______-==|        /`\\_. .__/\\ \\    | |  \\\\           _-~`         \n" +
+                            "       __--~~~  ,-/-==\\\\      (   | .  |~~~~|   | |   `\\        ,'             \n" +
+                            "    _-~       /'    |  \\\\     )__/==0==-\\<>/   / /      \\      /               \n" +
+                            "  .'        /       |   \\\\      /~\\___/~~\\/  /' /        \\   /'                \n" +
+                            " /  ____  /         |    \\`\\.__/-~~   \\  |_/'  /          \\/'                  \n" +
+                            "/-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`                   \n" +
+                            "                  \\_|      /        _) | ;  ),   __--~~                        \n" +
+                            "                    '~~--_/      _-~/- |/ \\   '-~ \\                            \n" +
+                            "                   {\\__--_/}    / \\\\_>-|)<__\\      \\                           \n" +
+                            "                   /'   (_/  _-~  | |__>--<__|      |                          \n" +
+                            "                  |   _/) )-~     | |__>--<__|      |                          \n" +
+                            "                  / /~ ,_/       / /__>---<__/      |                          \n" +
+                            "                 o-o _//        /-~_>---<__-~      /                           \n" +
+                            "                 (^(~          /~_>---<__-      _-~                            \n" +
+                            "                ,/|           /__>--<__/     _-~                               \n" +
+                            "             ,//('(          |__>--<__|     /  NATURAL 20 YO!  .----_          \n" +
+                            "            ( ( '))          |__>--<__|    |                 /' _---_~\\        \n" +
+                            "         `-)) )) (           |__>--<__|    |               /'  /     ~\\`\\      \n" +
+                            "        ,/,'//( (             \\__>--<__\\    \\            /'  //        ||      \n" +
+                            "      ,( ( ((, ))              ~-__>--<_~-_  ~--____---~' _/'/        /'       \n" +
+                            "    `~/  )` ) ,/|                 ~-_~>--<_/-__       __-~ _/                  \n" +
+                            "  ._-~//( )/ )) `                    ~~-'_/_/ /~~~~~~~__--~                    \n" +
+                            "   ;'( ')/ ,)(                              ~~~~~~~~~~                         \n" +
+                            "  ' ') '( (/                                                                   \n" +
+                            "    '   '  `");
+                    break;
+                }
+
             }
+
         }
 
+        System.out.println("Would you like to roll again?    Y/y or N/n  ");
+        userInput.next();
+        if(userInput.nextLine().equals("Y") || userInput.nextLine().equals("y")){
+            System.out.println("Let's roll!");
 
-
-
-
-
-
-
-
-
-
+        }
     }
-
-//}
-//
-//class Demo {
 
 }
